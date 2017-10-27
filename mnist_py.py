5c47cbb5f389c7401a44caf23e338e719f08070f
poulastyamukherjee <poulastyab@hotmail.co.uk>
2017-10-28 00:39:15 +0200

    Add files via upload

--- mnist_py.py
+++ mnist_py.py
@@ -17,7 +17,7 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.3).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 for _ in range(1000):
